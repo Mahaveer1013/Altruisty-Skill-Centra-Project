cec57d5ce55eb3abf6b7490c443f8a01e3b96c10
rahulj1011 <rahuljr2022@gmail.com>
2024-12-05 13:55:04 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -66,9 +66,10 @@ io.use(socketLoginRequired);
 socketRouter(io);
 
 // Connect to MongoDB and start the server
+const port = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    server.listen(5000, async() => {
+    server.listen(process.env.PORT, async() => {
       console.log('Server is running on port http://localhost:5000');
       insertCourses();
        insertDomains();
